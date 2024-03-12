e5f99eded0e29a045c1d93ee2a345ddc49cb845a
unknown <Aprendiz@BOGDFPCGMFN4162.FormacionSena.RED>
2024-03-12 17:05:27 -0500

    formularios1.0

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
-from . import app 
+from . import app , db 
 from .models import Medico
 from .models import Paciente
 from .models import Consultorio
 from .models import Cita
-from flask import render_template
+from flask import render_template, request
 
 #crear ruta para ver los medicos 
 @app.route("/medicos")
@@ -28,3 +28,44 @@ def get_all_consultorio():
 def get_all_cita():
     cita = Cita.query.all()
     return render_template("cita.html", cita=cita  )
+
+#cerar ruta traer el medico por id(get)
+@app.route("/medicos/<int:id>")
+def get_medico_by_id(id):
+    medico = Medico.query.get(id)
+    return render_template("medico.html", med = medico)
+
+
+#crear ruta para nuevo medico 
+@app.route("/medicos/create", methods = [ 'GET', 'POST' ])
+def create_medico():
+    #mostrar el formulario: metodo GET
+    if( request.method == 'GET'  ):
+        #el usuario ingreso con navegador con http://localhost:5000/medicos/create
+        especialidades = [
+            "cardiologia", 
+            "pediatria",
+            "oncologia"
+        ]
+        return render_template("medico_form.html",
+                            especialidades = especialidades)
+    
+
+
+#cuando el usuario presiona el boton de guardar 
+#los datos del formulario viajan al servidor 
+#utilizando el metodo post
+    elif(request.method== 'POST'):
+        #cuando se presiona 'guardar'
+        #crear un objeto de tipo medico 
+        new_medico = Medico(nombre = request.form["nombre"],
+                            apellidos = request.form["apellidos"],
+                            tipo_identificacion = request.form["ti"],
+                            numero_identificacion= request.form["ni"],
+                            registro_medico = request.form["rm"],
+                            especialidad = request.form["es"]
+                            )
+        #añadirlo a la sesion sqlalchemy
+        db.session.add(new_medico)
+        db.session.commit()
+        return "medico registrado" 
